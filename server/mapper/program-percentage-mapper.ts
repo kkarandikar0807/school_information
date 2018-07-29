e3f95ab42a71685a5cd4a81ce1b3653420728a1d
Kaushik Karandikar <kkarandikar0807@gmail.com>
2018-07-29 00:37:39 -0400

    mapper added

--- server/mapper/program-percentage-mapper.ts
+++ server/mapper/program-percentage-mapper.ts
@@ -1,9 +1,17 @@
-// import { ProgramPercentage } from "../models/program-percentage";
+import { ProgramPercentage } from "../models/program-percentage";
 
-// export class ProgramPercentageMapper {
-//     public static mapData(data: ProgramPercentage): ProgramPercentage {
+export class ProgramPercentageMapper {
+    public static mapData(data: ProgramPercentage): ProgramPercentage {
 
-//         let programPercentageData = new ProgramPercentage();
+        let programPercentageData = new ProgramPercentage();
+        programPercentageData.education = data.education;
+        programPercentageData.mathematics = data.mathematics;
+        programPercentageData.business_Marketing = data.business_marketing;
+        programPercentageData.communications_Technology = data.communications_technology;
+        programPercentageData.language = data.language;
+        programPercentageData.visual_Performing = data.visual_performing;
+        programPercentageData.engineering_Technology = data.engineering_technology;
 
-//     }
-// }
+
+    }
+}
--- server/models/program-percentage.ts
+++ server/models/program-percentage.ts
@@ -1,84 +1,350 @@
 export class ProgramPercentage {
 
-    constructor(
-        public education: number,
-        public mathematics: number,
-        public business_Marketing: number,
-        public communications_Technology: number,
-        public language: number,
-        public visual_Performing: number,
-        public engineering_Technology: number,
-        public parks_Recreation_Fitness: number,
-        public agriculture: number,
-        public security_Law_Enforcement: number,
-        public computer: number,
-        public precision_Production: number,
-        public humanities: number,
-        public library: number,
-        public psychology: number,
-        public social_Science: number,
-        public legal: number,
-        public english: number,
-        public construction: number,
-        public military: number,
-        public communication: number,
-        public public_Administration_Social_Service: number,
-        public architecture: number,
-        public ethnic_Cultural_Gender: number,
-        public resources: number,
-        public health: number,
-        public engineering: number,
-        public history: number,
-        public theology_Religious_Vocation: number,
-        public transportation: number,
-        public physical_Science: number,
-        public science_Technology: number,
-        public biological: number,
-        public family_Consumer_Science: number,
-        public philosophy_Religious: number,
-        public personal_Culinary: number,
-        public multidiscipline: number,
-        public mechanic_Repair_Technology: number
+    private _education: number;
+    private _mathematics: number;
+    private _business_Marketing: number;
+    private _communications_Technology: number;
+    private _language: number;
+    private _visual_Performing: number;
+    private _engineering_Technology: number;
+    private _parks_Recreation_Fitness: number;
+    private _agriculture: number;
+    private _security_Law_Enforcement: number;
+    private _computer: number;
+    private _precision_Production: number;
+    private _humanities: number;
+    private _library: number;
+    private _psychology: number;
+    private _social_Science: number;
+    private _legal: number;
+    private _english: number;
+    private _construction: number;
+    private _military: number;
+    private _communication: number;
+    private _public_Administration_Social_Service: number;
+    private _architecture: number;
+    private _ethnic_Cultural_Gender: number;
+    private _resources: number;
+    private _health: number;
+    private _engineering: number;
+    private _history: number;
+    private _theology_Religious_Vocation: number;
+    private _transportation: number;
+    private _physical_Science: number;
+    private _science_Technology: number;
+    private _biological: number;
+    private _family_Consumer_Science: number;
+    private _philosophy_Religious: number;
+    private _personal_Culinary: number;
 
-    ) {
-        this.education = education;
-        this.mathematics = mathematics;
-        this.business_Marketing = business_Marketing;
-        this.communications_Technology = communications_Technology,
-        this.language = language,
-        this.visual_Performing = visual_Performing,
-        this.engineering_Technology = engineering_Technology,
-        this.parks_Recreation_Fitness = parks_Recreation_Fitness,
-        this.agriculture = agriculture,
-        this.security_Law_Enforcement = security_Law_Enforcement,
-        this.computer = computer,
-        this.precision_Production = precision_Production,
-        this.humanities = humanities,
-        this.library = library,
-        this.psychology = psychology,
-        this.social_Science = social_Science,
-        this.legal = legal,
-        this.english = english,
-        this.construction = construction,
-        this.military = military,
-        this.communication = communication,
-        this.public_Administration_Social_Service = public_Administration_Social_Service,
-        this.architecture = architecture,
-        this.ethnic_Cultural_Gender = ethnic_Cultural_Gender,
-        this.resources = resources,
-        this.health = health,
-        this.engineering = engineering,
-        this.history = history,
-        this.theology_Religious_Vocation = theology_Religious_Vocation,
-        this.transportation = transportation,
-        this.physical_Science = physical_Science,
-        this.science_Technology = science_Technology,
-        this.biological = biological,
-        this.family_Consumer_Science = family_Consumer_Science,
-        this.philosophy_Religious = philosophy_Religious,
-        this.personal_Culinary = personal_Culinary,
-        this.multidiscipline = multidiscipline,
-        this.mechanic_Repair_Technology = mechanic_Repair_Technology
+    get education(): number {
+        return this._education;
     }
-    
+
+    set education(value: number) {
+        this._education = value;
+    }
+
+    get mathematics(): number {
+        return this._mathematics;
+    }
+
+    set mathematics(value: number) {
+        this._mathematics = value;
+    }
+
+    get business_Marketing(): number {
+        return this._business_Marketing;
+    }
+
+    set business_Marketing(value: number) {
+        this._business_Marketing = value;
+    }
+
+    get communications_Technology(): number {
+        return this._communications_Technology;
+    }
+
+    set communications_Technology(value: number) {
+        this._communications_Technology = value;
+    }
+
+    get language(): number {
+        return this._language;
+    }
+
+    set language(value: number) {
+        this._language = value;
+    }
+
+    get visual_Performing(): number {
+        return this._visual_Performing;
+    }
+
+    set visual_Performing(value: number) {
+        this._visual_Performing = value;
+    }
+
+    get engineering_Technology(): number {
+        return this._engineering_Technology;
+    }
+
+    set engineering_Technology(value: number) {
+        this._engineering_Technology = value;
+    }
+
+    get parks_Recreation_Fitness(): number {
+        return this._parks_Recreation_Fitness;
+    }
+
+    set parks_Recreation_Fitness(value: number) {
+        this._parks_Recreation_Fitness = value;
+    }
+
+    get agriculture(): number {
+        return this._agriculture;
+    }
+
+    set agriculture(value: number) {
+        this._agriculture = value;
+    }
+
+    get security_Law_Enforcement(): number {
+        return this._security_Law_Enforcement;
+    }
+
+    set security_Law_Enforcement(value: number) {
+        this._security_Law_Enforcement = value;
+    }
+
+    get computer(): number {
+        return this._computer;
+    }
+
+    set computer(value: number) {
+        this._computer = value;
+    }
+
+    get precision_Production(): number {
+        return this._precision_Production;
+    }
+
+    set precision_Production(value: number) {
+        this._precision_Production = value;
+    }
+
+    get humanities(): number {
+        return this._humanities;
+    }
+
+    set humanities(value: number) {
+        this._humanities = value;
+    }
+
+    get library(): number {
+        return this._library;
+    }
+
+    set library(value: number) {
+        this._library = value;
+    }
+
+    get psychology(): number {
+        return this._psychology;
+    }
+
+    set psychology(value: number) {
+        this._psychology = value;
+    }
+
+    get social_Science(): number {
+        return this._social_Science;
+    }
+
+    set social_Science(value: number) {
+        this._social_Science = value;
+    }
+
+    get legal(): number {
+        return this._legal;
+    }
+
+    set legal(value: number) {
+        this._legal = value;
+    }
+
+    get english(): number {
+        return this._english;
+    }
+
+    set english(value: number) {
+        this._english = value;
+    }
+
+    get construction(): number {
+        return this._construction;
+    }
+
+    set construction(value: number) {
+        this._construction = value;
+    }
+
+    get military(): number {
+        return this._military;
+    }
+
+    set military(value: number) {
+        this._military = value;
+    }
+
+    get communication(): number {
+        return this._communication;
+    }
+
+    set communication(value: number) {
+        this._communication = value;
+    }
+
+    get public_Administration_Social_Service(): number {
+        return this._public_Administration_Social_Service;
+    }
+
+    set public_Administration_Social_Service(value: number) {
+        this._public_Administration_Social_Service = value;
+    }
+
+    get architecture(): number {
+        return this._architecture;
+    }
+
+    set architecture(value: number) {
+        this._architecture = value;
+    }
+
+    get ethnic_Cultural_Gender(): number {
+        return this._ethnic_Cultural_Gender;
+    }
+
+    set ethnic_Cultural_Gender(value: number) {
+        this._ethnic_Cultural_Gender = value;
+    }
+
+    get resources(): number {
+        return this._resources;
+    }
+
+    set resources(value: number) {
+        this._resources = value;
+    }
+
+    get health(): number {
+        return this._health;
+    }
+
+    set health(value: number) {
+        this._health = value;
+    }
+
+    get engineering(): number {
+        return this._engineering;
+    }
+
+    set engineering(value: number) {
+        this._engineering = value;
+    }
+
+    get history(): number {
+        return this._history;
+    }
+
+    set history(value: number) {
+        this._history = value;
+    }
+
+    get theology_Religious_Vocation(): number {
+        return this._theology_Religious_Vocation;
+    }
+
+    set theology_Religious_Vocation(value: number) {
+        this._theology_Religious_Vocation = value;
+    }
+
+    get transportation(): number {
+        return this._transportation;
+    }
+
+    set transportation(value: number) {
+        this._transportation = value;
+    }
+
+    get physical_Science(): number {
+        return this._physical_Science;
+    }
+
+    set physical_Science(value: number) {
+        this._physical_Science = value;
+    }
+
+    get science_Technology(): number {
+        return this._science_Technology;
+    }
+
+    set science_Technology(value: number) {
+        this._science_Technology = value;
+    }
+
+    get biological(): number {
+        return this._biological;
+    }
+
+    set biological(value: number) {
+        this._biological = value;
+    }
+
+    get family_Consumer_Science(): number {
+        return this._family_Consumer_Science;
+    }
+
+    set family_Consumer_Science(value: number) {
+        this._family_Consumer_Science = value;
+    }
+
+    get philosophy_Religious(): number {
+        return this._philosophy_Religious;
+    }
+
+    set philosophy_Religious(value: number) {
+        this._philosophy_Religious = value;
+    }
+
+    get personal_Culinary(): number {
+        return this._personal_Culinary;
+    }
+
+    set personal_Culinary(value: number) {
+        this._personal_Culinary = value;
+    }
+
+    get multidiscipline(): number {
+        return this._multidiscipline;
+    }
+
+    set multidiscipline(value: number) {
+        this._multidiscipline = value;
+    }
+
+    get mechanic_Repair_Technology(): number {
+        return this._mechanic_Repair_Technology;
+    }
+
+    set mechanic_Repair_Technology(value: number) {
+        this._mechanic_Repair_Technology = value;
+    }
+
+    private _multidiscipline: number;
+    private _mechanic_Repair_Technology: number;
+
+   constructor() {
+
+   }
 }
